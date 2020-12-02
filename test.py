cd12699bbbdbcca0d0ffcec5aa81d3d603b8313b
Jiachun Xiang <jcxiang1230@gmail.com>
2020-12-01 19:56:18 -0500

    changed variables

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import time
 import math
 from math import trunc
 
-DIM = 0                 # Dimension of the board (n value)
+# DIM = 0                 # Dimension of the board (n value)
 board = []              # Stores the board
 row_conflicts = []      # Keeps track of row conflicts
 diagr_conflicts = []    # Keeps track of right diagonal conflicts
@@ -12,7 +12,7 @@ diagl_conflicts = []    # Keeps track of left diagonal conflicts
 
 # Read in the test n dimensions of the file into a list and return
 def readInFile():
-    file = open('nqueens.txt', 'r')
+    file = open('test.txt', 'r')
     dArrary = []
     for line in file:
         dArrary.append(int(line.rstrip('\n')))
@@ -34,21 +34,21 @@ def writeToFile():
 # When the parameter val as -1, it will subtract a conflict to the global lists
 # The values in the conflict arrays represent the number of queens in each row or diagonal
 #       (i.e. a count of 1 indicates no conflict because there is only one queen in the row or diagonal)
-def changeConflicts(col, row, val):
+def changeConflicts(col, row, val, dim):
     row_conflicts[row] += val
     diagr_conflicts[col + row] += val
-    diagl_conflicts[col + (DIM - row - 1)] += val
+    diagl_conflicts[col + (dim - row - 1)] += val
 
 
 # Finds the index of the best new queen position. Ties are broken randomly.
 # Parameter: the current column index
-def minConflictPos(col):
-    minConflicts = DIM
+def minConflictPos(col, dim):
+    minConflicts = dim
     minConflictRows = []
-    for row in range(DIM):
+    for row in range(dim):
         # calculate the number of conflicts using the conflict arrays
         conflicts = row_conflicts[row] + diagr_conflicts[col +
-                                                         row] + diagl_conflicts[col + (DIM - row - 1)]
+                                                         row] + diagl_conflicts[col + (dim - row - 1)]
         # if there are no conflicts in a row, immediately return that row value
         if conflicts == 0:
             return row
@@ -65,7 +65,7 @@ def minConflictPos(col):
 
 
 # Sets up the board using a greedy algorithm
-def createBoard():
+def createBoard(dim):
     global board
     global row_conflicts
     global diagr_conflicts
@@ -76,25 +76,25 @@ def createBoard():
 
     # Initialize the conflict arrays
     # The diagonal conflict lists are the size of the number of diagonals of the board
-    diagr_conflicts = [0] * ((2 * DIM) - 1)
-    diagl_conflicts = [0] * ((2 * DIM) - 1)
-    row_conflicts = [0] * DIM
+    diagr_conflicts = [0] * ((2 * dim) - 1)
+    diagl_conflicts = [0] * ((2 * dim) - 1)
+    row_conflicts = [0] * dim
 
     # Create an ordered set of all possible row values
-    rowSet = set(range(0, DIM))
+    rowSet = set(range(0, dim))
     # Create a list to keep track of which queens have not been placed
     notPlaced = []
 
-    for col in range(0, DIM):
+    for col in range(0, dim):
         # Pop the next possible row location to test
         testRow = rowSet.pop()
         # Calculate the conflicts for potential location
         conflicts = row_conflicts[testRow] + diagr_conflicts[col +
-                                                             testRow] + diagl_conflicts[col + (DIM - testRow - 1)]
+                                                             testRow] + diagl_conflicts[col + (dim - testRow - 1)]
         # If there are no conflicts, place a queen in that location on the board
         if conflicts == 0:
             board.append(testRow)
-            changeConflicts(col, board[col], 1)
+            changeConflicts(col, board[col], 1, dim)
         # If a conflict is found...
         else:
             # Place the potential row to the back of the set
@@ -103,11 +103,11 @@ def createBoard():
             testRow2 = rowSet.pop()
             # Calculate the conflicts
             conflicts2 = row_conflicts[testRow2] + diagr_conflicts[col +
-                                                                   testRow2] + diagl_conflicts[col + (DIM - testRow2 - 1)]
+                                                                   testRow2] + diagl_conflicts[col + (dim - testRow2 - 1)]
             # If there are no conflicts, place a queen in that location on the board
             if conflicts2 == 0:
                 board.append(testRow2)
-                changeConflicts(col, board[col], 1)
+                changeConflicts(col, board[col], 1, dim)
             else:
                 # Otherwise, add the possible row back to the set
                 rowSet.add(testRow2)
@@ -120,21 +120,21 @@ def createBoard():
         # Place the remaining queen locations
         board[col] = rowSet.pop()
         # Update the conflict lists
-        changeConflicts(col, board[col], 1)
+        changeConflicts(col, board[col], 1, dim)
 
 
 # Finds the column with the most conflicts
-def findMaxConflictCol():
+def findMaxConflictCol(dim):
     conflicts = 0
     maxConflicts = 0
     maxConflictCols = []
 
-    for col in range(0, DIM):
+    for col in range(0, dim):
         # Determine the row value for the current column
         row = board[col]
         # Calculate the number of conflicts using the conflict lists
         conflicts = row_conflicts[row] + diagr_conflicts[col +
-                                                         row] + diagl_conflicts[col+(DIM-row-1)]
+                                                         row] + diagl_conflicts[col+(dim-row-1)]
         # If conflicts are greater than the current max, make that column the maximum
         if (conflicts > maxConflicts):
             maxConflictCols = [col]
@@ -148,25 +148,29 @@ def findMaxConflictCol():
 
 
 # Sets up the board using createBoard() and then solves it with a min-conflict algorithm
-def solveNQueens():
-    createBoard()
+def solveNQueens(dim):
+    createBoard(dim)
     iteration = 0
-    maxIteration = 0.6 * DIM    # Define the maximum iterations as 0.6 * size of board
-
+    maxIteration = 0.6 * dim    # Define the maximum iterations as 0.6 * size of board
+    print("I am here to solve queens")
+    print(iteration)
+    print(maxIteration)
     while (iteration < maxIteration):
         # Calculate the maximum conflicting column and the number of conflicts it contains
-        col, numConflicts = findMaxConflictCol()
+        print("find max conflict col")
+        col, numConflicts = findMaxConflictCol(dim)
         # If the number of queens in the row, and diagonals is greater than 1 each (i.e. there are conflicts)
         if (numConflicts > 3):
-            # Use the minConflictPos() function to determine the row index with the least number of conflicts
-            newLocation = minConflictPos(col)
+            print("find new min")
+            newLocation = minConflictPos(col, dim)
             # If the better location is not its current location, switch the location
             if (newLocation != board[col]):
+                print("change location ")
                 # Remove the conflicts from the position the queen is leaving
-                changeConflicts(col, board[col], -1)
+                changeConflicts(col, board[col], -1, dim)
                 board[col] = newLocation
                 # Add a conflict to the position the queen is entering
-                changeConflicts(col, newLocation, 1)
+                changeConflicts(col, newLocation, 1, dim)
         # If the max number of conflicts (i.e. the number of queens in each row and diagonals) on the board
         #       equals 3, then there are no conflicts since the queen is alone in it's row and both diagonals
         elif numConflicts == 3:
@@ -181,7 +185,7 @@ def main():
 
     # Read in the file containing the DIM values
     dimensionArray = readInFile()
-
+    dim = 0
     for dimension in dimensionArray:
         # If the dimension is outside the constraints
         if dimension <= 3 or dimension > 10000000:
@@ -190,20 +194,21 @@ def main():
             writeToFile()
         else:
             # Set DIM equal to the current test dimension
-            DIM = dimension
+            dim = dimension
             # Start timer and set/reset boolen
             time0 = time.time()
             solved = False
             print("Searching for board configuration of size " +
                   str(dimension)+"...")
             # 6 is a special case, return hard-coded solution and skip while loop
-            if (DIM == 6):
+            if (dim == 6):
                 board = [1, 3, 5, 0, 2, 4]
                 solved = True
-            # Continues restarting solveNQueens() until a solution is found
-            while (not solved):
-                # Solved will be True when a solution is returned
-                solved = solveNQueens()
+            else:
+                while (not solved):
+                    # Solved will be True when a solution is returned
+                    print("I am going to solve queens")
+                    solved = solveNQueens(dim)
 
             print("Board configuration found for size " + str(dimension))
 
